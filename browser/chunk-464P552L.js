1e53e5b3a03cd861b2cdc5eaec8c279e19f124d6
Renzo <renzozegarra0317@gmail.com>
2025-11-30 21:35:40 -0500

    Auto-generated commit

---
+++ browser/chunk-464P552L.js
@@ -0,0 +1,1 @@
+var p={production:!1,apiUrl:"http://localhost:5000/api",apiBaseUrl:"http://localhost:5000",reniecApiToken:"439r89c4dLKM7i56IapVLpbAxRvM3dj9asM77U1WpmYlc9DlhuDhVndAzzkv"};export{p as a};
