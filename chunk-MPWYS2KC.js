f71e2f94ad2e927117eecf865a84d3ce6fd4438a
Renzo <renzozegarra0317@gmail.com>
2025-12-01 00:18:47 -0500

    Auto-generated commit

---
+++ chunk-MPWYS2KC.js
@@ -0,0 +1,1 @@
+var a={production:!0,apiUrl:"https://kala-aerobatic-luciana.ngrok-free.dev/api",apiBaseUrl:"https://kala-aerobatic-luciana.ngrok-free.dev",reniecApiToken:"439r89c4dLKM7i56IapVLpbAxRvM3dj9asM77U1WpmYlc9DlhuDhVndAzzkv"};export{a};
